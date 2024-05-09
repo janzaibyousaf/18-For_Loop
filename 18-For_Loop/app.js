418b8985fe0e49ebec2ad52dba0258d49a9e2875
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-05-09 11:48:34 +0500

    Add files via upload

---
+++ 18-For_Loop/app.js
@@ -0,0 +1,17 @@
+// var cityToCheck  = ("cleanestCities");
+
+// if (cityToCheck === "cleanestCities") {
+     
+//    alert("It's one of the cleanest cities");
+//  }console.log(cityToCheck)
+
+
+for (var a = 1; a <=10; a++) {
+
+  console.log("hello ");
+  
+}
+
+for ( var i = 1; i <=10 ; i++){
+console.log("hello")
+}
